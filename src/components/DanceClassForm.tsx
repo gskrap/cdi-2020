aecb1ccb1ed756933b3598253e4546a161f55cbb
George Skrapits <gskrap@gmail.com>
2020-06-07 18:51:32 -0400

    add teachers dropdown

--- src/components/DanceClassForm.tsx
+++ src/components/DanceClassForm.tsx
@@ -1,13 +1,32 @@
 import React from 'react';
 import {DanceClass} from '../models/DanceClass';
-import {IonInput, IonItem, IonLabel, IonList} from '@ionic/react';
+import {IonInput, IonItem, IonLabel, IonList, IonSelect, IonSelectOption} from '@ionic/react';
+import {AppState} from '../store/defaultStore';
+import {connect} from 'react-redux';
+import actions, {MappedActions} from '../actions/actions';
+import {User} from '../models/User';
 
 type DanceClassFormProps = {
   danceClass?: DanceClass;
+  teachers: User[] | null;
 };
 
-const DanceClassForm: React.FC<DanceClassFormProps> = ({ danceClass }) => {
-  const  [name, setName] = React.useState(danceClass ? danceClass.name : '') ;
+const DanceClassForm: React.FC<DanceClassFormProps & MappedActions<typeof  actions>> = ({ danceClass, teachers, actions }) => {
+  React.useEffect(() => {
+    if (!teachers) {
+      const fetchTeachers = async () => {
+        try {
+          await actions.fetchTeachers()
+        } catch (e) {
+          console.error(e)
+        }
+      };
+      fetchTeachers();
+    }
+  }, [teachers, actions]);
+
+  const [name, setName] = React.useState(danceClass ? danceClass.name : '');
+  const [teacherIds, setTeacherIds] = React.useState(danceClass ? danceClass.teachers.map(t => t.id) : []);
 
   return (
     <IonList>
@@ -15,8 +34,21 @@ const DanceClassForm: React.FC<DanceClassFormProps> = ({ danceClass }) => {
         <IonLabel position="floating">Class Name</IonLabel>
         <IonInput value={name} onIonChange={e => setName(e.detail.value!)}></IonInput>
       </IonItem>
+
+      <IonItem>
+        <IonLabel position="floating">Teachers</IonLabel>
+        <IonSelect value={teacherIds} multiple={true} cancelText="Cancel" okText="OK" onIonChange={e => setTeacherIds(e.detail.value)}>
+          {teachers && teachers.map((teacher, i) => (
+            <IonSelectOption key={i} value={teacher.id}>{`${teacher.first_name} ${teacher.last_name}`}</IonSelectOption>
+          ))}
+        </IonSelect>
+      </IonItem>
     </IonList>
   );
 };
 
-export default DanceClassForm;
+const mapState = (state: AppState) => {
+  return { teachers: state.teachers }
+};
+
+export default connect(mapState, actions)(DanceClassForm);
